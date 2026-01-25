0768dcba22ece685812b93a26734f80c3d7bbec0
B-Gilb022 <benjamin.gilbert2004@icloud.com>
2026-01-24 19:49:41 -0500

    Small Fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 // Pre-generated falling items to avoid Math.random() during render
